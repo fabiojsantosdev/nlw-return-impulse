0db4eb1239a3bd20d3a133c2822e936f5a51eafd
m1n31rO <fabio_santos_493@hotmail.com>
2022-05-10 09:25:25 -0300

    Projeto Finalizado

---
+++ README.md
@@ -0,0 +1,9 @@
+# Next Level Week #8: Return
+
+![nlw-heat-impulse-logo](./archive/nlw-return.png)
+
+<p align="center">Evento da Rocketseat - 02 a 06 de maio de 2022</p>
+
+## Trilha Impulse
+
+
---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer';
+
+import { MailAdapter, SendMailData } from '../mail-adapter';
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '36c690a11f93ba',
+    pass: 'f4658b82887f83',
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Matheus Lemma <contatomatheuslemma@gmail.com>',
+      subject,
+      html: body,
+    });
+  }
+}
