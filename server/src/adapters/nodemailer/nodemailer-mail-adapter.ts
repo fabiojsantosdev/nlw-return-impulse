9d7ecc0ece669313e4dabb073791b067befc7b6a
m1n31rO <fabio_santos_493@hotmail.com>
2022-05-10 10:18:34 -0300

    Mail aterado

--- server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -6,8 +6,8 @@ const transport = nodemailer.createTransport({
   host: 'smtp.mailtrap.io',
   port: 2525,
   auth: {
-    user: '36c690a11f93ba',
-    pass: 'f4658b82887f83',
+    user: "12ef553b614d67",
+    pass: "7a3c7e6d1197d5"
   },
 });
 
@@ -15,7 +15,7 @@ export class NodemailerMailAdapter implements MailAdapter {
   async sendMail({ subject, body }: SendMailData) {
     await transport.sendMail({
       from: 'Equipe Feedget <oi@feedget.com>',
-      to: 'Matheus Lemma <contatomatheuslemma@gmail.com>',
+      to: 'Fábio Santos <fabio.santos.493@gmail.com>',
       subject,
       html: body,
     });
